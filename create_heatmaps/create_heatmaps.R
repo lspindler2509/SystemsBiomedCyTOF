8f3859a19ef917db533258ebac0de7963c9c6b83
lspindler2509 <lisa.spindler@hotmail.de>
2025-02-24 10:04:52 +0100

    correct final filepath

--- create_heatmaps/create_heatmaps.R
+++ create_heatmaps/create_heatmaps.R
@@ -5,7 +5,7 @@ library(pheatmap)
 library(RColorBrewer)
 
 
-filepath <- "create_heatmaps/metrics_all_against_all_original_with_weighted__intersection.csv" # file with computed metric scores
+filepath <- "create_heatmaps/metrics_all_against_all_original_with_weighted_no_intersection" # file with computed metric scores
 metrics_all <- fread(filepath)
 
 # modify metrics df
